1574fdbfa0c90c6d342320b69607cd3713b71797
Ados <109715319+Ados43@users.noreply.github.com>
2025-10-03 22:54:40 +1000

    Delete directory - Wrong location

--- Source Code/src/main/java/org/oosd/teamorange_finalsubmission/HighScoreEntry.java
+++
@@ -1,9 +0,0 @@
-/// HighScoreEntry.java
-// Data model representing a single high score.
-// Used for persistence and display in HighScoreView.
-
-package org.oosd.teamorange_finalsubmission;
-
-// High score record
-public record HighScoreEntry(String name, int score, long timestamp, String config) {
-}
